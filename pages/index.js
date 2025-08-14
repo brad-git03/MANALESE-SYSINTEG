a132b327faad5e3fdbbce53d877983d8afaab492
brad-git03 <bsmanalese.student@ua.edu.ph>
2025-08-14 12:03:24 +0800

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+ 
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello! I'm <strong>Brad</strong>, I enjoy building scalable and user-friendly applications and am always looking for new challenges and opportunities to learn.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
